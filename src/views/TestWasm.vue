8ac5fe507a784249effbbbbfd4b6ad3302e92312
firu11 <firu@firu.dev>
2026-01-21 18:47:28 +0100

    fix wasm testing

--- src/views/TestWasm.vue
+++ src/views/TestWasm.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { CalendarCore } from '@/wasm/core-wrapper';
 import { onMounted } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+import { DateTime } from 'luxon';
 
 onMounted(async () => {
   // let e: CalendarEvent = await CalendarCore.getEvent(1);
@@ -11,7 +13,17 @@ onMounted(async () => {
   <button @click="CalendarCore.initialize()">Init</button>
   <button @click="CalendarCore.delete()">Delete</button>
   <button @click="CalendarCore.clone('https://github.com/firu11/personal-web')">Clone</button>
-  <button @click="CalendarCore.addEvent({ id: 1, from: new Date(), to: new Date(), title: 'Meeting', location: '' })">
+  <button
+    @click="
+      CalendarCore.addEvent({
+        id: uuidv4(),
+        from: DateTime.now(),
+        to: DateTime.now().plus({ hour: 2 }),
+        title: 'Meeting',
+        location: '',
+      })
+    "
+  >
     Add Event
   </button>
 </template>

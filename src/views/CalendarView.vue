78becbfb472221d54868b2586c90d30bc6ac1969
firu11 <firu@firu.dev>
2026-01-25 15:54:56 +0100

    fix start of the week

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -2,20 +2,20 @@
 import WeekDisplay from '@/components/WeekDisplay.vue';
 import { useSettings } from '@/composables/useSettings';
 import { computed, ref } from 'vue';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 
 const { settings } = useSettings();
 
 const viewName = ref(settings.value.defaultView);
 
-const d = DateTime.now();
+const today = DateTime.now();
 
 const startOfTheWeek = computed(() => {
-  const todaysDay = d.weekday;
-  const diff = d.day - todaysDay + (todaysDay === 1 ? -6 : 1);
-  const monday = d.minus(diff);
-  monday.set({ hour: 0, minute: 0, second: 0, millisecond: 0 }); // maybe not needed
-  return monday;
+  const diff = Math.abs(settings.value.weekStart - today.weekday);
+  const startDate = today.minus(Duration.fromObject({ days: diff }));
+
+  startDate.set({ hour: 0, minute: 0, second: 0, millisecond: 0 }); // maybe not needed
+  return startDate;
 });
 </script>
 

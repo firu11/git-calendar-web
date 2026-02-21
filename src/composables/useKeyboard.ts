d4bb81676a2fccbb78fc270efe0edbd764840b8b
firu11 <firu@firu.dev>
2026-02-21 19:17:12 +0100

    fix keyboard shortcuts blocking when input is focused

--- src/composables/useKeyboard.ts
+++ src/composables/useKeyboard.ts
@@ -5,10 +5,17 @@ import { moveView } from '@/utils';
 import { DateTime } from 'luxon';
 
 export function useKeyboard() {
+  function inputNeededElsewhere(): boolean {
+    return document.activeElement!.matches(
+      'input, textarea, select, [contenteditable], button, [role="textbox"], [role="combobox"]',
+    );
+  }
+
   // after mounted/ready cuz of router
   onMounted(() => {
     // T -> go to today
     onKeyStroke('t', (e) => {
+      if (inputNeededElsewhere()) return;
       e.preventDefault();
       const today = DateTime.now();
       router.replace({ params: { year: today.year, month: today.month, day: today.day } });
@@ -16,24 +23,28 @@ export function useKeyboard() {
 
     // W -> switch to week view
     onKeyStroke('w', (e) => {
+      if (inputNeededElsewhere()) return;
       e.preventDefault();
       router.replace({ params: { view: 'week' } });
     });
 
     // M -> switch to month view
     onKeyStroke('m', (e) => {
+      if (inputNeededElsewhere()) return;
       e.preventDefault();
       router.replace({ params: { view: 'month' } });
     });
 
     // move view back
     onKeyStroke('ArrowLeft', (e) => {
+      if (inputNeededElsewhere()) return;
       e.preventDefault();
       moveView(true, router);
     });
 
     // move view forward
     onKeyStroke('ArrowRight', (e) => {
+      if (inputNeededElsewhere()) return;
       e.preventDefault();
       moveView(false, router);
     });

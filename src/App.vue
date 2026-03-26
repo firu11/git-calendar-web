887b132cd20b119768af3429287e83bbe3e6e22c
firu11 <firu@firu.dev>
2026-03-26 17:04:55 +0100

    move alpha tag

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
 <template>
   <main>
-    <span id="alpha">Alpha version</span>
     <RouterView />
   </main>
 </template>
@@ -10,18 +9,5 @@
 <style scoped>
 main {
   height: 100%;
-}
-
-#alpha {
-  position: absolute;
-  top: 1rem;
-  left: calc(var(--sidebar-width) + 1rem);
-  padding: 0.1rem 0.3rem;
-
-  font-size: 0.7rem;
-  font-weight: bold;
-  color: var(--git-color);
-  border: 1px solid var(--git-color);
-  border-radius: 2rem;
 }
 </style>
--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -48,6 +48,7 @@ function updateCallDown() {
 </script>
 
 <template>
+  <span id="alpha">Alpha version</span>
   <div id="calendar-view">
     <SideBar>
       <MonthSideMap />
@@ -63,6 +64,19 @@ function updateCallDown() {
 </template>
 
 <style scoped>
+#alpha {
+  position: absolute;
+  top: 1rem;
+  left: calc(var(--sidebar-width) + 1rem);
+  padding: 0.1rem 0.3rem;
+
+  font-size: 0.7rem;
+  font-weight: bold;
+  color: var(--git-color);
+  border: 1px solid var(--git-color);
+  border-radius: 2rem;
+}
+
 #calendar-view {
   display: grid;
   grid-template-columns: var(--sidebar-width) auto;
